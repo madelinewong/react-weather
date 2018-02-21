4331b10de064fc59e58aff6a33515d2701a10bd8
Madeline Wong <madelinegwong@outlook.com>
2018-02-20 20:56:31 -0500

    added forms and a button.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-    <div> hello</div>
+  <div>
+    <h1> React Weather </h1>
+    <p> Enter your latitude and longitude to get the weather!</p>
+    <form>
+      <label> 
+        Latitude:
+        <input type="text"/>
+      </label>
+      <label>
+        Longitude: 
+        <input type="text"/>
+      </label>
+      <button type="submit"> Get the Weather! </button>
+    </form>
+  </div>
     );
   }
 }
