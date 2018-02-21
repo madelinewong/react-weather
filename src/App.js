b7dea0ddb80274c2a0c2580f60fa93019fc4055c
Madeline Wong <madelinegwong@outlook.com>
2018-02-20 21:06:37 -0500

    added the lat and lon input fields with event handlers.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      lat: 0,
+      lon: 0,
+    }; 
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLonChange = this.handleLonChange.bind(this);
+  }
+
+  handleLatChange(e){
+    this.setState({
+      lat: +e.target.value
+  })
+}
+  handleLonChange(e){
+    this.setState({
+      lon: +e.target.value
+    })
+  }
   render() {
     return (
   <div>
@@ -10,11 +30,15 @@ class App extends Component {
     <form>
       <label> 
         Latitude:
-        <input type="text"/>
+        <input type="text" 
+        onChange={(e) => this.handleLatChange(e)} 
+        value={this.state.lat}/>
       </label>
       <label>
         Longitude: 
-        <input type="text"/>
+        <input type="text" 
+        onChange={(e) => this.handleLonChange(e)} 
+        value={this.state.lon}/>
       </label>
       <button type="submit"> Get the Weather! </button>
     </form>
