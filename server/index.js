6a0df2bbf8490cfe6aa69b982c9c1c4df0428833
Madeline Wong <madelinegwong@outlook.com>
2018-02-21 20:26:15 -0500

    made the request of weather data through the proxy service dynamic based on the route params lat and lon.

--- server/index.js
+++ server/index.js
@@ -11,15 +11,16 @@ const serverApp = express();
 const port = process.env.PORT || 5000;
 
 serverApp.get("/forecast/:lat,:lon", function(request, response) {
+  const { lat, lon } = request.params;
   const url =
-    `https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`;
+    `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
   axios
     .get(url)
     .then(res => {
-      response.json(res.data);
+      response.status(200).json(res.data);
     })
     .catch(err => {
-      response.json({
+      response.status(500).json({
         msg: "your stuff is broke."
       });
     });
