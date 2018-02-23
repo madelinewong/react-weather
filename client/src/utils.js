0c508e139ffc2bfac0e55e5adffefede7174c6b7
Madeline Wong <madelinegwong@outlook.com>
2018-02-22 19:57:31 -0500

    added a simplified day and timestamp.

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,2 +1,13 @@
 
 export const isEmptyObject = (obj) => Object.keys(obj).length === 0;
+
+export const convertTimestamp = (seconds) => {
+    const d = new Date(seconds * 1000); //convert to ms
+    //format: mm/dd/YYYY HH:MM 
+    const year = d.getFullYear();
+    const month = d.getMonth();
+    const date = d.getDate();
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
+    return `${month + 1}/${date}/${year} ${hours}:${minutes}`;
+}
